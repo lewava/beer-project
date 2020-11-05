1b7d7127e89ea5e92e40457f266699b03e15c104
Leon Wass Vallin <leon.wass.vallin@mail.com>
2020-11-05 18:10:59 +0100

    search page is working, some small bugs left.

--- javascript/search-page.js
+++ javascript/search-page.js
@@ -4,70 +4,51 @@ const ul = document.querySelector(".list");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const counter = document.querySelector(".counter");
+const select = document.querySelector("select");
+let currentPage = 1;
 
 document.querySelector(".close").addEventListener("click", closeList);
 checkInput();
- /*** vart är du? LEOOOON, skolan ekar tomt på leons */
-/**
- * Lägg till validation.
- */
+
+function getData(input) {
+  switch (select.value) {
+    case "name":
+      return fetch(`https://api.punkapi.com/v2/beers?beer_name=${input}`);
+    case "hops":
+      return fetch(`https://api.punkapi.com/v2/beers?hops=${input}`);
+    case "malt":
+      return fetch(`https://api.punkapi.com/v2/beers?malt=${input}`);
+    case "brewed-before":
+      return fetch(`https://api.punkapi.com/v2/beers?brewed_before=${input}`);
+    case "brewed-after":
+      return fetch(`https://api.punkapi.com/v2/beers?brewed_after=${input}`);
+    case "abv-greater":
+      return fetch(`https://api.punkapi.com/v2/beers?abv_gt=${input}`);
+    case "abv-lesser":
+      return fetch(`https://api.punkapi.com/v2/beers?abv_lt=${input}`);
+    default:
+      alert("Please select a search method");
+  }
+}
+
 function checkInput() {
   document.querySelector(".search").addEventListener("click", () => {
-    ul.innerHTML = "";
     let filteredInput = input.value.trim().replace(" ", "_");
 
-    if (filteredInput !== '') {
-      if (isNaN(input)) {
-        const fetchName = fetch(
-          `https://api.punkapi.com/v2/beers?beer_name=${filteredInput}`
-        );
-/*         const fetchHops = fetch(
-          `https://api.punkapi.com/v2/beers?hops=${filteredInput}`
-        );
-        const fetchMalt = fetch(
-          `https://api.punkapi.com/v2/beers?malt=${filteredInput}`
-        );
-        const fetchBB = fetch(
-          `https://api.punkapi.com/v2/beers?brewed_before=${filteredInput}`
-        );
-        const fetchBA = fetch(
-          `https://api.punkapi.com/v2/beers?brewed_after=${filteredInput}`
-        ); */
-
-        Promise.all([fetchName])
-          .then((values) => {
-            return Promise.all(values.map((resp) => resp.json()));
-          })
-          .then((data) => {
-            let beerNames = getBeerNames(data);
-            changeCurrentPage(beerNames);
-            addItems(beerNames);
-          })
-          .catch((err) => err.message);
-      } else {
-        const fetchAG = fetch(
-          `https://api.punkapi.com/v2/beers?abv_gt=${filteredInput}`
-        );
-        const fetchAL = fetch(
-          `https://api.punkapi.com/v2/beers?abv_lt=${filteredInput}`
-        );
-
-        Promise.all([fetchAG, fetchAL])
-          .then((values) => {
-            return Promise.all(values.map((resp) => resp.json()));
-          })
-          .then((data) => {
-            let beerNames = getBeerNames(data);
-            changeCurrentPage(beerNames);
-            addItems(beerNames);
-          })
-          .catch((err) => err.message);
-      }
-
-      ulContainer.style.display = "flex";
-      input.value = "";
-    }else {
-      alert('Input field is empty.');
+    if (filteredInput !== "") {
+      getData(filteredInput)
+        .then((resp) => resp.json())
+        .then((data) => {
+          let beerNames = getBeerNames(data);
+          console.log(beerNames);
+          changeCurrentPage(beerNames);
+          addItems(beerNames, currentPage);
+          ulContainer.style.display = "flex";
+          input.value = "";
+        })
+        .catch((err) => err.message);
+    } else {
+      alert("Input field is empty.");
     }
   });
 }
@@ -75,54 +56,64 @@ function checkInput() {
 function getBeerNames(beers) {
   let beerNames = [];
   beers.forEach((element) => {
-    element.forEach((element) => {
-      beerNames.push(element.name);
-    });
+    beerNames.push(element.name);
   });
   return beerNames;
 }
 
 function changeCurrentPage(beerNames) {
-  let currentPage = 1;
   let numberOfPages = Math.ceil(beerNames.length / 10);
 
   if (numberOfPages === 0) {
     numberOfPages = 1;
   }
 
-  counter.textContent = currentPage;
   document.querySelector(".pages").textContent = numberOfPages;
 
   left.addEventListener("click", () => {
     if (currentPage === 1) {
-      currentPage = 1;
+      counter.textContent = currentPage;
+      addItems(beerNames, currentPage);
     } else {
       currentPage -= 1;
       counter.textContent = currentPage;
+      addItems(beerNames, currentPage);
     }
   });
 
   right.addEventListener("click", () => {
     if (currentPage === numberOfPages) {
-      currentPage = numberOfPages;
+      counter.textContent = currentPage;
+      addItems(beerNames, currentPage);
     } else {
       currentPage += 1;
       counter.textContent = currentPage;
+      addItems(beerNames, currentPage);
     }
   });
 }
 
-function addItems(beerNames) {
-  for (let i = 0; i < 10; i++) {
-    li = document.createElement("li");
-    li.className = "li";
-    li.textContent = beerNames[i];
-    ul.appendChild(li);
+function addItems(beerNames, currentPage) {
+  ul.innerHTML = "";
+  if (currentPage === 1) {
+    for (let i = 0; i < 10; i++) {
+      li = document.createElement("li");
+      li.className = "li";
+      li.textContent = beerNames[i];
+      ul.appendChild(li);
+    }
+  } else {
+    const number = Math.round(currentPage) * 10 - 10;
+    for (let i = number; i < number + 10; i++) {
+      li = document.createElement("li");
+      li.className = "li";
+      li.textContent = beerNames[i];
+      ul.appendChild(li);
+    }
   }
 }
 
 function closeList() {
   document.querySelector(".list").innerHTML = "";
   ulContainer.style.display = "none";
-  counter.textContent = 1;
 }
